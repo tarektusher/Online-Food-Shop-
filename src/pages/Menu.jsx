d454042b21f27a5cd78ab0ce275fef15a9ddc8c4
Md Tarek Hossain Tusher <57767316+tarektusher@users.noreply.github.com>
2023-09-11 20:21:17 +0600

    Update Menu Pages and creating left navbar

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -3,11 +3,31 @@ import { Link } from 'react-router-dom';
 const Menu = () =>{
      return (
        <>
-         <div className="subnav">
-            <Link to="/breakfast">Breakfast</Link>
-            <Link to="/lunch">Lunch</Link>
-            <Link to="/dinner">Dinner</Link>
-            <Link to="/snacks">Snacks</Link>
+        <div className='nav'>
+          <h1>Khabo Dhabo Food Shop</h1>
+          <h3>We provide always fresh food for our customer</h3>
+        </div>
+         <div className='subnav'>
+              <div className="subnav-link">
+              <i class='bx bxs-right-arrow first-item'>
+                <Link to="/breakfast">Breakfast</Link>
+              </i>
+              <i class='bx bxs-right-arrow'>
+                <Link to="/lunch">Lunch</Link>
+              </i>
+              <i class='bx bxs-right-arrow'>
+                <Link to="/snacks">Snacks</Link>
+              </i>
+              <i class='bx bxs-right-arrow'>
+                <Link to="/dinner">Dinner</Link>
+              </i>
+              </div>
+              <div className='subnav-img'>
+                  <img src="\src\assets\pexels-fauxels-3184183.webp" alt="" />
+                  <img src="\src\assets\pexels-flat-hito-904349.webp" alt="" />
+                  <img src="\src\assets\pexels-mali-maeder-65175.webp" alt="" />
+                  <img src="\src\assets\pexels-oliver-sjöström-1025804.webp" alt="" />
+              </div>
          </div>
        </>
      )
